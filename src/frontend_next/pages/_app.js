7f573dad7691c2466441ea23797f6d88e43b9ac2
Rifqi Naufal Abdjul <rifqi.abdjul23@gmail.com>
2022-04-29 07:50:18 +0700

    Add website title and favicon 👍

--- src/frontend_next/pages/_app.js
+++ src/frontend_next/pages/_app.js
@@ -1,12 +1,19 @@
 import theme from "../styles/theme";
 import { ChakraProvider } from "@chakra-ui/provider";
 import "@fontsource/cabin/500.css";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>DNATrain - Hanarebanare no machi o tsunagu ressha wa itte shimatta ne</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 };
 
